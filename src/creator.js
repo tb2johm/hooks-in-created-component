d08b3e3d05322e29ee8a95168a012baeeac61c84
Mike Ash <mike.ash@semcon.com>
2021-02-25 19:36:54 +0100

    Avoid pass in object to the selector by using "useCallback" and moving out the call outside condition(switch).

--- src/creator.js
+++ src/creator.js
@@ -1,33 +1,30 @@
-import Test from './Test';
+import React, { useCallback } from "react";
+import Test from "./Test";
 
 const Dashboard = (ui) => {
-  return (
-    <div>
-      {ui}
-    </div>
-  )
-}
+  return <div>{ui}</div>;
+};
 
 const Ui = () => {
-  return (<div>{['a', 'b'].map(t => Test(t))}</div>);
-}
+  return <div>{["a", "b"].map((t) => Test(t))}</div>;
+};
 
 const Settings = () => <h1>Settings</h1>;
 
 const Render = (content) => {
+  const ui = useCallback(() => ["a", "b"].map((t) => Test(t)), []);
   switch (content) {
-    case 'ui':
+    case "ui":
       //WORKS
       //return Ui;
 
       //DOESN'T WORK
-      const ui = ['a', 'b'].map(t => Test(t));
-      return () => Dashboard(ui);
-    case 'settings':
+      return ui;
+    case "settings":
       return Settings;
     default:
-      return <div/>
+      return <div />;
   }
-}
+};
 
 export default Render;
