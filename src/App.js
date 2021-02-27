394e2dc3c35b6bf61d943c683ef06ccf1eca648a
Markus Johansson <markus.johansson@semcon.com>
2021-02-27 21:26:51 +0100

    Added working and not working examples

--- src/App.js
+++ src/App.js
@@ -1,31 +1,70 @@
 import { BrowserRouter, NavLink, Route } from 'react-router-dom';
 import './App.css';
-import Render from './creator';
+import { Render } from './creator';
 import { useDispatch } from 'react-redux';
 
-const Uii = (routing) => {
+const CreateNav = (recip) => (
+  <div style={{ width:'100%', backgroundColor:'darkgrey', color:'white' }}>
+    <ul style={{ display:'flex', listStyleType:'none' }}>
+      {recip.map(route => <li style={{ display:'inline', padding:'0 10px' }}><NavLink to={route.path}>{route.name}</NavLink></li>)}
+    </ul>
+  </div>
+);
+
+const CreateRoutes = (recip) => {
   const dispatch = useDispatch();
   let i = 0;
 
   return (
-    <BrowserRouter>
-      {routing.map(route => (i++ === 0 ?
-        <Route exact path={route.path} component={Render(route.content)}/> :
-        <Route path={route.path} component={Render(route.content)}/>
+    <div>
+      {recip.map(section => (i++ === 0 ?
+        <Route exact path={section.path} component={Render(section.content)}/> :
+        <Route path={section.path} component={Render(section.content)}/>
       ))}
 
       <button onClick={() => dispatch({type:'incA'})}>+</button>
-      {/*<NavLink to='/'><div><span>Comp</span></div></NavLink>
-      <NavLink to='/settings/'><div><span>Settings</span></div></NavLink>*/}
-    </BrowserRouter>
+    </div>
   );
 };
 
 function App() {
-  const routing = [{path: '/', name: 'home', content: 'ui'}, {path: '/settings', name: 'Settings', content: 'settings'}];
-  const Ui = () => Uii(routing);
+  //The recip comes from a json config file, but is simplified here
+  const recip = [{
+    path: '/',
+    name: "Doesn't work",
+    content: {
+      type: 'ui', // Change this to ui-callback or ui-no-callback-but-works to see it work
+      variables: ['a', '1']
+    }
+  }/*, { // for some reason it doesn't work when I have them all created at the same time.
+    path: '/2',
+    name: "Works with callback",
+    content: {
+      type: 'ui-callback',
+      variables: ['a', '2']
+    }
+  }, { // I guess that I really don't understand how stuff (functions) is created...
+    path: '/3',
+    name: "Works without callback",
+    content: {
+      type: 'ui-no-callback-but-works',
+      variables: ['a', '3']
+    }
+  }*/, {
+    path: '/settings',
+    name: 'Settings',
+    content: {
+      type: 'settings'
+    }
+  }];
+
+  const Ui = () => CreateRoutes(recip);
+  const Nav = () => CreateNav(recip);
   return (
-    <Ui/>
+    <BrowserRouter>
+      <Nav/>
+      <Ui/>
+    </BrowserRouter>
   );
 }
 
--- src/creator.js
+++ src/creator.js
@@ -1,34 +1,44 @@
 import React, { useCallback } from "react";
 import Test from "./Test";
 
-const Dashboard = (ui) => {
-  return <div>{ui}</div>;
-};
-
-const Ui = () => {
-  return <div>{["a", "b"].map((t) => Test(t))}</div>;
-};
-
+const Dashboard = (ui) => <div>{ui}</div>;
 const Settings = () => <h1>Settings</h1>;
-const Render = (content) => {
-  const ui = useCallback(() =>
-    Dashboard(
-      ["a", "b"].map((t) => Test(t)),
-      []
-    )
-  );
-  switch (content) {
-    case "ui":
-      //WORKS
-      //return Ui;
 
-      //DOESN'T WORK
-      return ui;
+const Ui = (variables) => Dashboard(
+  variables.map((t) => Test(t))
+);
+
+
+export const Render = (content) => {
+  const UiCallback = useCallback((variables) =>
+    Dashboard(
+      variables.map((t) => Test(t)),
+    ),[]
+  );
+
+  switch (content.type) {
+    case "ui":
+      // DOESN'T WORK
+      // This was the first attempt done.
+      // When updating the value the full Dashboard is rerendered
+      const ui = content.variables.map((t) => Test(t))
+      return () => Dashboard(ui);
+
+    case "ui-callback":
+      // WORKS
+      // After lots of investiations and trial and error
+      // we found this callback that works.
+      return () => UiCallback(content.variables)
+
+    case "ui-no-callback-but-works":
+      // ALSO WORKS
+      // Now I also found by chance that this also works
+      return () => Ui(content.variables);
+
     case "settings":
       return Settings;
+
     default:
       return <div />;
   }
 };
-
-export default Render;
